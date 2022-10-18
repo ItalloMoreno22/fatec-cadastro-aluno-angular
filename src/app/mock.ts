d232f0dd0a826332db4597c3326232b4bc1ca66b
ItalloMoreno22 <Itallorodrigues2628@gmail.com>
2022-10-18 08:51:30 -0300

    first commit

---
+++ src/app/mock.ts
@@ -0,0 +1,14 @@
+import { students } from "./students";
+
+export const STUDENTS: students[] = [
+{name: 'Antonio Tadeu', age: 20, id: 1, course: 'ADS' },
+{name: 'Maria Arruda', age: 25, id: 2, course: 'GTI' },
+{name: 'Marcelo Mendes', age: 27, id: 3, course: 'MEC' },
+{name: 'Thiago Mesquita', age: 20, id: 4, course: 'ADS' },
+{name: 'Richard Souza', age: 30, id: 5, course: 'GTI' },
+{name: 'Glauco Todesco', age: 42, id: 6, course: 'GE' },
+{name: 'Margarida', age: 22, id: 7, course: 'MAT' },
+{name: 'Juliana Verona', age: 55, id: 8, course: 'ADS' },
+{name: 'Pedro Sanchez', age: 21, id: 9, course: 'Eventos' },
+{name: 'Amanda Marques', age: 19, id: 10, course: 'ADS' },
+];
---
+++ src/app/students.ts
@@ -0,0 +1,7 @@
+export interface students{
+    name: string;
+    age: number
+    id: number;
+    course: string;
+
+}
